93db8b46ec9c4efa5dac43d4e7cd859cac8b88d7
Tomasz Rojek <tomekr@gmail.com>
2019-05-15 12:21:50 +0200

    Add logic to script

--- js/script.js
+++ js/script.js
@@ -2,3 +2,16 @@ var computerMove, playerMove;
 playerMove = 'papier';
 computerMove = 'kamień';
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to' + playerMove + 'to wygrywasz');
+var playerInput, playerMove;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+} else if (playerInput == '3') {
+  playerMove = 'nożyce';
+} else {
+  playerMove = 'nieznany ruch';
+}
+printMessage('Twój ruch: ' + playerMove);
