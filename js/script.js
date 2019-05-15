5094e87ed4977ae699aeea89080d6057196cda70
Tomasz Rojek <tomekr@gmail.com>
2019-05-15 10:40:23 +0200

    Add new variable

--- js/script.js
+++ js/script.js
@@ -1,9 +1,4 @@
-function printMessage(msg){
-	var div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
-}
-
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
+var computerMove, playerMove;
+playerMove = 'papier';
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to' + playerMove + 'to wygrywasz');
